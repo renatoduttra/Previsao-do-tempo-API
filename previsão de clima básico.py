4d9bd19ba3e7e516c4f86fa3842c37d35dafda13
renatoduttra <renatoduttra@gmail.com>
2022-04-19 23:32:02 -0300

    Create previsão de clima básico.py

---
+++ previsão de clima básico.py
@@ -0,0 +1,13 @@
+import requests
+
+# link do open_weather: https://openweathermap.org/
+
+API_KEY = "coloque sua API aqui"
+cidade = "rio de janeiro"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
+
+requisicao = requests.get(link)
+requisicao_dic = requisicao.json()
+descricao = requisicao_dic['weather'][0]['description']
+temperatura = requisicao_dic['main']['temp'] - 273.15
+print(descricao, f"{temperatura}ºC")
